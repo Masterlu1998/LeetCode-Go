b0bd4a0fe08a6f852d59e2d295e2bcd19578df67
lb <lubing3302@gmail.com>
2019-06-25 17:03:29 +0800

    feat: 增加leetcode题目：“位1的个数”解题记录

--- main.go
+++ main.go
@@ -4,37 +4,6 @@ import (
 	"fmt"
 )
 
-type Queue []int 
-
-func New() *Queue {
-	q := make(Queue, 0)
-	return &q
-}
-
-func (q *Queue)Push(x int) {
-	*q = append(*q, x)
-} 
-
-func (q *Queue)Pop() int {
-	result := (*q)[0]
-	*q = (*q)[1:]
-	return result
-}
-
-func (q *Queue)Peek() int {
-	return (*q)[0]
-} 
-
-
 func main() {
-	q := New()
-	q.Push(1)
-	q.Push(2)
-	q.Push(3)
-	fmt.Println(q.Peek())
-	fmt.Println(q.Pop())
-	fmt.Println(q.Pop())
-	fmt.Println(q.Peek())
-
+	fmt.Println(9 & 8)
 }
-
