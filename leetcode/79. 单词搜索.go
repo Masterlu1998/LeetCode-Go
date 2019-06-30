8f1bc08018a218568f74a8fb411abd83a5bb6797
lb <lubing3302@gmail.com>
2019-06-30 15:18:03 +0800

    fix: 修改79.单词搜索解题记录

--- leetcode/79. 单词搜索.go
+++ leetcode/79. 单词搜索.go
@@ -24,12 +24,12 @@ package leetcode
 
 var (
     width, height int
-    cache map[[2]int]bool
+    cacheExi map[[2]int]bool
 ) 
 
-func exist(board [][]byte, word string) bool {
+func existFunc2(board [][]byte, word string) bool {
     title := word[0]
-    cache = make(map[[2]int]bool)
+    cacheExi = make(map[[2]int]bool)
     width = len(board[0])
     height = len(board)
     result := false
@@ -37,16 +37,16 @@ func exist(board [][]byte, word string) bool {
     for i := 0; i < height; i++ {
         for j := 0; j < width; j++ {
             if board[i][j] == title {
-                result = result || _exist(board, word, 0, j, i)
-                delete(cache, [2]int{j, i})
+                result = result || _existFunc2(board, word, 0, j, i)
+                delete(cacheExi, [2]int{j, i})
             }
         }
     }
     return result
 }
 
-func _exist(board [][]byte, word string, index, w, h int) bool {
-    cache[[2]int{w, h}] = true
+func _existFunc2(board [][]byte, word string, index, w, h int) bool {
+    cacheExi[[2]int{w, h}] = true
     if board[h][w] != word[index] {
         return false
     }
@@ -60,25 +60,76 @@ func _exist(board [][]byte, word string, index, w, h int) bool {
     
     result := false
     
-    if w - 1 >= 0 && !cache[[2]int{w-1, h}] {
-        result = result || _exist(board, word, index+1, w-1, h)
-        delete(cache, [2]int{w-1, h})
+    if w - 1 >= 0 && !cacheExi[[2]int{w-1, h}] {
+        result = result || _existFunc2(board, word, index+1, w-1, h)
+        delete(cacheExi, [2]int{w-1, h})
     }
     
-    if w + 1 < width && !cache[[2]int{w+1, h}] {
-        result = result || _exist(board, word, index+1, w+1, h)
-        delete(cache, [2]int{w+1, h})
+    if w + 1 < width && !cacheExi[[2]int{w+1, h}] {
+        result = result || _existFunc2(board, word, index+1, w+1, h)
+        delete(cacheExi, [2]int{w+1, h})
     }
     
-    if h - 1 >= 0 && !cache[[2]int{w, h-1}] {
-        result = result || _exist(board, word, index+1, w, h-1)
-        delete(cache, [2]int{w, h-1})
+    if h - 1 >= 0 && !cacheExi[[2]int{w, h-1}] {
+        result = result || _existFunc2(board, word, index+1, w, h-1)
+        delete(cacheExi, [2]int{w, h-1})
     }
     
-    if h + 1 < height && !cache[[2]int{w, h+1}] {
-        result = result || _exist(board, word, index+1, w, h+1)
-        delete(cache, [2]int{w, h+1})
+    if h + 1 < height && !cacheExi[[2]int{w, h+1}] {
+        result = result || _existFunc2(board, word, index+1, w, h+1)
+        delete(cacheExi, [2]int{w, h+1})
     }
     
+    return result
+}
+
+// 思路2：原地修改数组来标记已经遍历过的位置，节省创建map的开销
+
+func existFunc1(board [][]byte, word string) bool {
+    title := word[0]
+    width = len(board[0])
+    height = len(board)
+    result := false
+    
+    for i := 0; i < height; i++ {
+        for j := 0; j < width; j++ {
+            if board[i][j] == title {
+                result = result || _existFunc1(board, word, 0, j, i)
+                board[i][j] = word[0]
+            }
+        }
+    }
+    return result
+}
+
+func _existFunc1(board [][]byte, word string, index, w, h int) bool {
+    if board[h][w] != word[index] {
+        return false
+    }
+    
+    if index == len(word) - 1 {
+        return true
+    }
+    
+    result := false
+    tmp := board[h][w]
+    board[h][w] = '#'
+    if w - 1 >= 0 {
+        result = result || _existFunc1(board, word, index+1, w-1, h)
+    }
+    
+    if w + 1 < width {
+        result = result || _existFunc1(board, word, index+1, w+1, h)
+    }
+    
+    if h - 1 >= 0 {
+        result = result || _existFunc1(board, word, index+1, w, h-1)
+    }
+    
+    if h + 1 < height {
+        result = result || _existFunc1(board, word, index+1, w, h+1)
+    }
+    
+    board[h][w] = tmp
     return result
 }
