d9c49ef9aa3dc0ba83fc71ac519fb06f7dff33cf
robbin <lubing3302@gmail.com>
2019-10-07 19:16:07 +0800

    feat: 增加leetcode题目：“71. 简化路径”解题记录

--- main.go
+++ main.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"algorithm-go/algorithm_map"
-)
-
 func main() {
-	algorithm_map.TestAdjacencyMatrixDFS()
-	fmt.Println("//////////////")
-	algorithm_map.TestAdjacencyTableDFS()
-	fmt.Println("//////////////")
-	algorithm_map.TestAdjacencyMatrixBFS()
-	fmt.Println("//////////////")
-	algorithm_map.TestAdjacencyTableBFS()
 }
